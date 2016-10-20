96aaa749462c4e0505dbd2003c6120376a76ce2b
surajpotnuru <surajpotnuru7@gmail.com>
2016-10-20 20:01:34 +0530

    Dot Navigator

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 //Variables
 
 var gallerySideBarOpen = true;
+var page = 1;
 
-
+$(".dots-navigator .dot:first-child").addClass('dot-selected');
 
 //Nav Menu Click Event Listeners
 
@@ -17,15 +18,53 @@ $(".close-modal").click(function(event){
 $("#galleryLink").click(function(event){
     event.stopPropagation();
     $("#gallery").css("top",0);
+    $(".dots-navigator").find(".dot-selected").removeClass('dot-selected');
+    $(".dot2").addClass('dot-selected');
     $(".gallery-side-bar-toggler").addClass("open");
+});
+$("#teamLink").click(function(event){
+    event.stopPropagation();
+    $("#team").css("top",0);
 });
 
 $(".gallery-side-bar-toggler").click(function(){
     if(gallerySideBarOpen){
         $(".gallery-side-bar-toggler").removeClass("open");
+        $(".gallery-side-bar-toggler .stripe").css("background","#fff");
+        $(".gallery-side-bar").css("width","0%");
+        $(".gallery-wrapper").css("width","95%");
+
         gallerySideBarOpen = false;
     }else{
         $(".gallery-side-bar-toggler").addClass("open");
+        $(".gallery-side-bar-toggler .stripe").css("background","#1565C0");
+        $(".gallery-side-bar").css("width","20%");
+        $(".gallery-wrapper").css("width","75%");
+
         gallerySideBarOpen = true;
     }
+});
+
+//Dots navigator click listeners
+
+$(".dot1").click(function(event){
+    event.stopPropagation();
+    if(page==2){
+        $("#gallery").css("top","100vh");
+    }
+    $(".dots-navigator").find(".dot-selected").removeClass('dot-selected');
+    $(this).addClass('dot-selected');
+});
+
+$(".dot2").click(function(event){
+    event.stopPropagation();
+    page = 2;
+    $("#gallery").css("top",0);
+    $(".gallery-side-bar-toggler").addClass("open");
+    $(".dots-navigator").find(".dot-selected").removeClass('dot-selected');
+    $(this).addClass('dot-selected');
+});
+
+$(".dot3").click(function(){
+
 });
