09c0ca24ff6c40a1adf744dc9bec929cee8cd479
surajpotnuru <surajpotnuru7@gmail.com>
2016-10-22 14:36:04 +0530

    Base Setup Completed

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 //Variables
 
 var gallerySideBarOpen = true;
+var teamSideBarOpen = true;
 var page = 1;
 
 $(".dots-navigator .dot:first-child").addClass('dot-selected');
+$(".gallery-side-bar-toggler").addClass("open");
+$(".team-side-bar-toggler").addClass("open");
 
 //Nav Menu Click Event Listeners
 
@@ -20,11 +23,14 @@ $("#galleryLink").click(function(event){
     $("#gallery").css("top",0);
     $(".dots-navigator").find(".dot-selected").removeClass('dot-selected');
     $(".dot2").addClass('dot-selected');
-    $(".gallery-side-bar-toggler").addClass("open");
+    page =2;
 });
 $("#teamLink").click(function(event){
     event.stopPropagation();
     $("#team").css("top",0);
+    $(".dots-navigator").find(".dot-selected").removeClass('dot-selected');
+    $(".dot3").addClass('dot-selected');
+    page =3;
 });
 
 $(".gallery-side-bar-toggler").click(function(){
@@ -44,13 +50,31 @@ $(".gallery-side-bar-toggler").click(function(){
         gallerySideBarOpen = true;
     }
 });
+$(".team-side-bar-toggler").click(function(){
+    if(teamSideBarOpen){
+        $(".team-side-bar-toggler").removeClass("open");
+        $(".team-side-bar-toggler .stripe").css("background","#fff");
+        $(".team-side-bar").css("width","0%");
+        $(".team-wrapper").css("width","95%");
+
+        teamSideBarOpen = false;
+    }else{
+        $(".team-side-bar-toggler").addClass("open");
+        $(".team-side-bar-toggler .stripe").css("background","#2E7D32");
+        $(".team-side-bar").css("width","20%");
+        $(".team-wrapper").css("width","75%");
+
+        teamSideBarOpen = true;
+    }
+});
 
 //Dots navigator click listeners
 
 $(".dot1").click(function(event){
     event.stopPropagation();
-    if(page==2){
+    if(page==2 || page == 3){
         $("#gallery").css("top","100vh");
+        $("#team").css("top","200vh");
     }
     $(".dots-navigator").find(".dot-selected").removeClass('dot-selected');
     $(this).addClass('dot-selected');
@@ -58,13 +82,23 @@ $(".dot1").click(function(event){
 
 $(".dot2").click(function(event){
     event.stopPropagation();
+    if(page == 3 ){
+        $("#team").css("top","200vh");
+    }
     page = 2;
     $("#gallery").css("top",0);
-    $(".gallery-side-bar-toggler").addClass("open");
     $(".dots-navigator").find(".dot-selected").removeClass('dot-selected');
     $(this).addClass('dot-selected');
 });
 
 $(".dot3").click(function(){
 
+    if(page == 2 ){
+        $("#gallery").css("top","100vh");
+    }
+
+    page = 3;
+    $("#team").css("top",0);
+    $(".dots-navigator").find(".dot-selected").removeClass('dot-selected');
+    $(this).addClass('dot-selected');
 });
