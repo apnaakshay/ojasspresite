44060a09619ac3cbb93d037c3ed8fb8a1b308824
surajpotnuru <surajpotnuru7@gmail.com>
2016-10-22 23:09:06 +0530

    font awesome cdn used

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+
+particlesJS.load('particles-js', '../particlesConfig.json', function() {
+  console.log('callback - particles.js config loaded');
+});
+
 //Variables
 
 var gallerySideBarOpen = true;
